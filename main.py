37f0c63ecf9077204be08f8500b1304fb2fc4c5c
Ayush Chakravarthy <ayush.k.chakravarthy@gmail.com>
2020-11-18 09:55:09 -0600

    completed

--- main.py
+++ main.py
@@ -1,26 +1,33 @@
-import sys
-import numpy as np
-import torch
+import argparse
+import torch.utils.data
+from torchvision import datasets, transforms
 
-from utils import load_data
-from utils import config_init
-
+from trainer import TrainerVaDE
+from utils import get_mnist
 
 if __name__ == '__main__':
-    dataset = 'none'
-    assert len(sys.argv) == 2
-    arg = sys.argv[1]
-    if arg in ['mnist', 'reuters10k', 'har']:
-        dataset = arg
-    else:
-        print("Enter a valid dataset\n")
-    ispretrain = False
-    # copied over from the DEC paper
-    batch_size = 100
-    latent_dim = 10
-    intermediate_dim = [500, 500, 2000]
-    torch.set_default.dtype('float32')
-    accuracy = []
-    X, Y = load_data(dataset)
-    input_dim, epoch, n_classes, lr_nn, lr_gmm, decay_n, decay_nn, decay_gmm, alpha, data_type = config_init(dataset)
-    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--epochs", type=int, default=300,
+                        help="number of iterations")
+    parser.add_argument("--patience", type=int, default=50,
+                        help="Patience for Early Stopping")
+    parser.add_argument('--lr', type=float, default=2e-3,
+                        help='learning rate')
+    parser.add_argument("--batch_size", type=int, default=100,
+                        help="Batch size")
+    parser.add_argument('--pretrain', type=bool, default=True,
+                        help='learning rate')
+    parser.add_argument('--pretrained_path', type=str, default='weights/pretrained_parameter.pth',
+                        help='Output path')
+    args = parser.parse_args()
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print(device)
+
+    dataloader = get_mnist(batch_size=args.batch_size)
+
+    vade = TrainerVaDE(args, device, dataloader)
+    if args.pretrain==True:
+        vade.pretrain()
+    vade.train()
+
--- trainer.py
+++ trainer.py
@@ -1,10 +1,14 @@
 import math
+import numpy as np
 import torch
 from torch import optim
 import torch.nn.functional as F
+from sklearn.mixture import GaussianMixture
+from scipy.optimize import linear_sum_assignment as linear_assignment
 
 
 from models import Autoencoder, VaDE
+
 
 def weights_init(m):
     classname = m.__class__.__name__
@@ -40,5 +44,105 @@ class Trainer:
         self.save_weights_for_VaDE()
 
     def train_GMM(self):
+        # changes for each dataset
         print('Fitting GMM')
+        # might not work
+        x = torch.cat([data[0] for data in self.dataloader]).view(-1, 784).to(self.device)
+        z = self.autoencoder.encode(x)
+        self.gmm = GaussianMixture(n_components=10, covariance_type='diag')
+        self.gmm.fit(z.cpu().detach().numpy())
+
+    def save_weights_VaDE(self):
+        print("Saving Weights.")
+        state_dict = self.autoencoder.state_dict()
+
+        self.VaDE.load_state_dict(state_dict, strict=False)
+        self.VaDE.pi_prior_data = torch.from_numpy(self.gmm.weight_).float().to(self.device)
+        self.VaDE.mu_prior_data = torch.from_numpy(self.gmm.means_).float().to(self.device)
+        self.VaDE.log_var_prior_data = torch.log(torch.from_numpy(self.gmm.covariances_)).float().to(self.device)
+        torch.save(self.VaDE.state_dict(), self.args.pretrained_path)
+
+    def train(self):
+        if self.args.pretrain is True:
+            self.VaDE.load_state_dict(torch.load(self.args.pretrained_path,
+                                                 map_location=self.device))
+
+        else:
+            self.VaDE.apply(weights_init)
+
+        self.optimizer = optim.Adam(self.VaDE.parameters(), lr=self.args.lr)
+        lr_scheduler = torch.optim.lr_scheduler.StepLR(
+            self.optimizer, step_size=10, gamma=0.9)
+
+        print("Training VaDE")
+        
+        for epoch in range(self.args.epochs):
+            self.train_VaDE(epoch)
+            self.test_VaDE(epoch)
+            lr_scheduler.step()
+
+    def train_VaDE(self, epoch):
+        self.VaDE.train()
+
+        total_loss = 0
+        for x, _ in self.dataloader:
+            self.optimizer.zero_grad()
+            x = x.to(self.device)
+            x_hat, mu, log_var, z = self.VaDE(x)
+            loss = self.compute_loss(x, x_hat, mu, log_var, z)
+            loss.backward()
+            self.optimizer.step()
+            total_loss += loss.item()
+        print("Training VaDE Epoch: {}, Loss: {}".format(epoch, total_loss))
+
+    def test_VaDE(self, epoch):
+        self.VaDE.eval()
+        with torch.no_grad():
+            total_loss = 0
+            y_true, y_pred = [], []
+            for x, true in self.dataloader:
+                x = x.to(self.device)
+                x_hat, mu, log_var, z = self.VaDE(x)
+                gamma = self.compute_gamma(z, self.VaDE.pi_prior)
+                pred = torch.argmax(gamma, dim=1)
+                loss = self.compute_loss(x, x_hat, mu, log_var, z)
+                total_loss += loss.item()
+                y_true.extend(true.numpy())
+                y_pred.extend(pred.cpu().detach().numpy())
+
+            acc = self.cluster_acc(np.array(y_true), np.array(y_pred))
+            print("Testing VaDE... Epoch: {}, Loss: {}, Acc: {}".format(epoch, total_loss, acc[0]))
+
+    def compute_loss(self, x, x_hat, mu, log_var, z):
+        p_c = self.VaDE.pi_prior
+        gamma = self.compute_gamma(z, p_c)
+
+        log_p_x_given_z = F.binary_cross_entropy(x_hat, x, reduction='sum')
+        h = log_var.exp().unsqueeze(1) + (mu.unsqueeze(1) - self.VaDE.mu_prior).pow(2)
+        h = torch.sum(self.VaDE.log_var_prior + h / self.VaDE.log_var_prior.exp(), dim=2)
+        log_p_z_given_c = 0.5 * torch.sum(gamma * h)
+        log_p_c = torch.sum(gamma * torch.log(p_c + 1e-9))
+        log_q_c_given_x = torch.sum(gamma * torch.log(gamma + 1e-9))
+        log_q_z_given_x = 0.5 * torch.sum(1 + log_var)
+
+        loss = log_p_x_given_z + log_p_z_given_c - log_p_c + log_q_c_given_x - log_q_z_given_x
+        loss /= x.size(0)
+        return loss
+
+    def compute_gamma(self, z, p_c):
+        h = (z.unsqueeze(1) - self.VaDE.mu_prior).pow(2) / self.VaDE.log_var_prior.exp()
+        h += self.VaDE.log_var_prior
+        h += torch.Tensor([np.log(np.pi*2)]).to(self.device)
+        p_z_c = torch.exp(torch.log(p_c + 1e-9).unsqueeze(0) - 0.5 * torch.sum(h, dim=2)) + 1e-9
+        gamma = p_z_c / torch.sum(p_z_c, dim=1, keepdim=True)
+        return gamma
+
+    def cluster_acc(self, real, pred):
+        D = max(pred.max(), real.max())+1
+        w = np.zeros((D, D), dtype=np.int64)
+        for i in range(pred.size):
+            w[pred[i], real[i]] += 1
+        ind = linear_assignment(w.max() - w)
+        return sum([w[i, j] for i, j in ind])*1.0/pred.size*100, w
+
 
